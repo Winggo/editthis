148c3c3bb494cc321e36c8911adc62a77e8ae9fa
winggo <winggo.tse@gmail.com>
2017-11-13 17:02:37 -0800

    implmented color slider

--- src/pages/drawpic.js
+++ src/pages/drawpic.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Sheet from '../components/sheet';
+import {SliderPicker} from 'react-color';
 
 class DrawPic extends React.Component {
+
   render() {
     return (
       <div>
         You've Reached the Route!!!!!!!!!!!!!!!!!!!
         <Sheet imageURL = '/api/images/serve/testKor.jpg'/>
+        <SliderPicker onChange={this.handleChange}/>
       </div>
     );
   }
