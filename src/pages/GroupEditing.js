b18e0519c66408871eac903a40d1799befe44267
Matthew Lo <mamlo@ucsc.edu>
2017-11-29 19:54:13 -0800

    Updated GroupEditing and newGroup

--- src/pages/GroupEditing.js
+++ src/pages/GroupEditing.js
@@ -3,10 +3,58 @@ import {Link} from 'react-router-dom';
 import Api from '../helpers/api';
 
 class GroupEditing extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      tool: 0
+    }; 
+  }
+
+  changeCursor(cur, num) {
+      console.log(this);
+      document.body.style.cursor = cur;
+      this.setState({tool: num}, () => {console.log(this.state.tool);
+      });
+  }
+
   render() {
+
+  	const containerStyle = {
+      width: '30%',
+  		'flex-direction': 'row',
+      'flex-wrap': 'wrap',
+  		backgroundColor: 'DodgerBlue'
+  	};
+
+  	const flexDiv = {
+  		backgroundColor: '#f1f1f1',
+  		width: '40px',
+      height: '40px',
+  		padding: '10px',
+  		margin: '10px',
+  		textAlign: 'center',
+  		lineHeight: '20px',
+  		fontSize: '20px'
+  	};
+  	
     return (
-      <div>
-        Group Editing Page
+      <div id = "parent" style = {{display: 'flex', backgroundColor: 'DodgerBlue'}}>
+    	 <div style = {{display: 'flex', flexDirection:'column'}}>
+    	   <button style = {flexDiv} onClick = {() => this.changeCursor("pointer", 1)}>1</button>
+    	   <button style = {flexDiv} onClick = {() => this.changeCursor("crosshair", 2)}>2</button>
+    	   <button style = {flexDiv}>3</button>
+    	   <button style = {flexDiv}>4</button>
+    	   <button style = {flexDiv}>5</button>
+    	   <button style = {flexDiv}>6</button>
+    	   <button style = {flexDiv}>7</button>
+    	   <button style = {flexDiv}>8</button>
+      </div>
+
+    	<div>
+        <Sheet imageURL = '/api/images/serve/testKor.jpg'/>
+        <SliderPicker onChange={this.handleChange}/>
+      </div>
       </div>
     );
   }
