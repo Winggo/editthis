e5080360ab23180f999a1de0a1e1edb8a588705c
chrisjmok <christophermok@Christophers-MacBook-Pro-3.local>
2017-11-29 21:00:05 -0800

    added color picker and fixed dotting bug

--- src/components/sheet.js
+++ src/components/sheet.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Upload from '../pages/upload';
-import Home from '../pages/home';
 import Styles from '../styles';
+import tools from '../helpers/pen';
 
 class Sheet extends React.Component {
   constructor() {
     super();
-    this.args = {lines:[],
+    this.args = {actions:[],
                  dragging: false,
                  lineNum: 0,
-                 thickns: 10
+                 thickns: 10,
+                 natWidth:0,
+                 natHeight:0,
+                 width:0,
+                 height:0,
+                 cornerX:0,
+                 cornerY:0,
+                 context:null,
+                 canvas:null,
+                 image:null,
                 }
-    //index of last line
-    this.lineNum = 0;
-    this.canvas = null;
-    this.context = null;
-    //true when mouse is down
-    this.dragging = false;
-    this.image = null;
-    this.width = 0;
-    this.height = 0;
-    this.natWidth = 0;
-    this.natHeight = 0;
-    this.cornerX = 0;
-    this.cornerY = 0;
-    this.thickns = 10;
   }
+
+
   componentDidMount(){
     this.args.canvas = document.getElementById('myCanvas');
     this.args.context = this.args.canvas.getContext('2d');
@@ -49,14 +45,13 @@ class Sheet extends React.Component {
         this.args.height = 500;
         this.args.cornerX  = (500-this.args.width)/2;
       }
-
-      this.args.context.drawImage(
-        this.args.image,
-        0,0,this.args.natWidth,this.args.natHeight,
-        this.args.cornerX, this.args.cornerY,this.args.width, this.args.height
-      );
+      this.drawBase(this.args.context);
     }
     console.log("mounted");
+  }
+
+  shouldComponenetUpdate(){
+    return false;
   }
   //returns mouse position relative to canvas
   getMousePos(canvas, ev){
@@ -69,59 +64,112 @@ class Sheet extends React.Component {
   
   //when mouse held down starts line
   handleMouseDown(ev){
-    const mousePos = this.getMousePos(this.args.canvas, ev);
-    const lineStart = [];
-    lineStart.push(mousePos);
-    this.args.lines.push(lineStart);
-    this.args.lineNum = this.args.lines.length - 1;
-    this.args.dragging = true;
-    this.draw();
-    console.log(this.args.points + " startLine");
+    console.log(tools);
+    tools.penMouseDown(this, ev);
   }
 
+  // penMouseDown(ev){
+  //   const mousePos = this.getMousePos(this.args.canvas, ev);
+  //   const lineStart = {type: 'line',
+  //                      points: [],
+  //                      color: this.props.color};
+  //   lineStart.points.push(mousePos);
+  //   this.args.actions.push(lineStart);
+  //   this.args.lineNum = this.args.actions.length - 1;
+  //   this.args.dragging = true;
+  //   this.draw(this.args.context,this.args.actions);
+  // }
+
   handleMouseMove(ev){
+     this.penMouseMove(ev);
+  }
+
+  penMouseMove(ev){
     if(this.args.dragging){
       const mousePos = this.getMousePos(this.args.canvas, ev);
-      this.args.lines[this.args.lineNum].push(mousePos); 
-      this.draw();
-    } 
+      this.args.actions[this.args.lineNum].points.push(mousePos); 
+      this.draw(this.args.context,this.args.actions);
+    }
   }
 
   handleMouseUp(){
     this.args.dragging = false;
-    console.log(this.args.lines);
   }
 
-  point(x, y, canvas){
-    canvas.beginPath();
-    canvas.arc(x, y, 1, 0, 2 * Math.PI);
-    canvas.fill();
+  dot(x, y, context, color){
+    console.log("dot");
+    console.log("color   " + color);
+    context.beginPath();
+    context.arc(x, y, this.args.thickns/2, 0, 2 * Math.PI);
+    context.fillStyle = color;
+    context.fill();
   }
-  //draws the image with lines
-  draw(){
-    this.args.context.drawImage(
+
+  drawBase(context){
+     context.drawImage(
         this.args.image,
         0,0,this.args.natWidth,this.args.natHeight,
         this.args.cornerX, this.args.cornerY,this.args.width,this.args.height
     );
-    let line = 0;
-    this.args.context.lineWidth = this.args.thickns;
-    this.args.context.beginPath();
+  }
+//DELETE IF NEVER USED
+  drawLines(context,actions){
+    let lineNum = 0;
+    context.lineWidth = this.args.thickns;
     //draw every line
-    for(line; line<this.args.lines.length; line++){
+    for(lineNum; lineNum<actions.length; lineNum++){
       //start at first point in line
-      let startX = this.args.lines[line][0].x;
-      let startY = this.args.lines[line][0].y;
-      this.point(startX, startY, this.args.context);
-      this.args.context.moveTo(startX, startY);
-      let point = 1;
-      for (point; point<this.args.lines[line].length; point++){
-      //line to every next point in line
-        this.args.context.lineTo(
-          this.args.lines[line][point].x, this.args.lines[line][point].y
+      let startX = actions[lineNum].points[0].x;
+      let startY = actions[lineNum].points[0].y;
+      this.dot(startX, startY, context, actions[lineNum].color);
+      context.beginPath();
+      context.moveTo(startX, startY);
+      let pointNum = 1;
+      context.strokeStyle = actions[lineNum].color;
+      for (pointNum; pointNum<actions[lineNum].points.length; pointNum++){
+      //line to every next pointNum in line
+        context.lineTo(
+          actions[lineNum].points[pointNum].x, actions[lineNum].points[pointNum].y
         );
       }
-      this.args.context.stroke();
+      //this.dot(actions[lineNum].points[pointNum-1].x, actions[lineNum].points[pointNum-1].y, context, actions[lineNum].color);
+      context.stroke();
+    }
+  }
+
+  drawSingleLine(points, color, context,){
+      console.log("drawSingleLine");
+      console.log(points);
+      console.log(color);
+      let startX = points[0].x;
+      let startY = points[0].y;
+      this.dot(startX, startY, context, color);
+      context.lineWidth = this.args.thickns;
+      context.beginPath();
+      context.moveTo(startX, startY);
+      let pointNum = 1;
+      context.strokeStyle = color;
+      for (pointNum; pointNum<points.length; pointNum++){
+      //line to every next pointNum in line
+        context.lineTo(
+          points[pointNum].x, points[pointNum].y
+        );
+      }
+      context.stroke();
+      this.dot(points[pointNum-1].x, points[pointNum-1].y, context, color);
+     
+  }
+  
+
+  //draws the image with lines
+  draw(context, actions){
+    console.log("main draw function");
+    this.drawBase(context);
+    let actionNum = 0;
+    for(actionNum; actionNum<actions.length; actionNum++){
+      if(actions[actionNum].type == 'line'){
+        this.drawSingleLine(actions[actionNum].points, actions[actionNum].color, context);
+      }
     }
   }
 
