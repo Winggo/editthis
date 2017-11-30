3a8c852d9f26f96b5f96ac1078da0f2612a8402f
chrisjmok <chrisjmok@gmail.com>
2017-11-30 03:25:06 -0800

    removed changes now that user.email works

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#mysql -h127.0.0.1 -uroot -e 'DROP DATABASE editthis;'
+mysql -h127.0.0.1 -uroot -e 'DROP DATABASE editthis;'
 mysql -h127.0.0.1 -uroot -e 'CREATE DATABASE editthis;'
 mysql -h127.0.0.1 -uroot -e "$(cat initial.sql)" editthis
