1621cbd6f6ce33960a0e3e64ac90152792f65a6b
Cheekyshroom <phernima@ucsc.edu>
2017-12-04 00:19:34 -0500

    Added nicer test wrapper

--- src/tests/test.sh
+++ src/tests/test.sh
@@ -3,7 +3,7 @@
 # Reset Database
 mysql -h127.0.0.1 -uroot -e 'DROP DATABASE IF EXISTS editthis;'
 mysql -h127.0.0.1 -uroot -e 'CREATE DATABASE editthis;'
-mysql -h127.0.0.1 -uroot -e "$(cat ../../initial.sql)" editthis
+mysql -h127.0.0.1 -uroot -e "$(cat initial.sql)" editthis
 
 # Group creation test 
 groupId="1234"
